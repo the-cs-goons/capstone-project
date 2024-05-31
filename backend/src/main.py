dbdd2ce3c7352dc9b303748ddfff7a37bc9e54bd
Josh Quail <josh@madbean.com>
2024-05-31 15:12:32 +1000

    test update to trigger workflow

--- backend/src/main.py
+++ backend/src/main.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     if os.getenv("CS3900_ENV") == "production":
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         ssl_context.load_cert_chain(keyfile="/usr/src/ssl/tls.key", certfile="/usr/src/ssl/tls.crt")
-        uvicorn.run(app, host="0.0.0.0", port=8443, log_level="warning", ssl_keyfile="/usr/src/ssl/tls.key", ssl_certfile="/usr/src/ssl/tls.crt")
+        uvicorn.run(app, host="0.0.0.0", port=8443, log_level="info", ssl_keyfile="/usr/src/ssl/tls.key", ssl_certfile="/usr/src/ssl/tls.crt")
     else:
-        uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
+        uvicorn.run(app, host="0.0.0.0", port=8080, log_level="debug")
