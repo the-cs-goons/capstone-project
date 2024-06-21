d58e69b653e875cd35dd12a232718c84c19e0c09
malb27 <yuyangw03@gmail.com>
2024-06-21 21:44:24 +1000

    Basic structure of issuer agent & relevant endpoints

--- issuer-lib/issuer/credential_issuer.py
+++ issuer-lib/issuer/credential_issuer.py
@@ -1,13 +1,27 @@
+from typing import Any
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-from .models.hello_world import HelloWorldResponse
+from .models.responses import RequestResponse, UpdateResponse
 
 
 class CredentialIssuer:
-    async def hello_world(self) -> HelloWorldResponse:
-        return HelloWorldResponse(hello="Hello", world="World")
+    async def landing(self):
+        return {"message": "Hello World"}
+
+    async def recieve_credential_request(self, cred_type: str) -> Any:
+        return {"ticket": 1,
+                "link": cred_type}
+
+    async def credential_status(self, token: str) -> Any:
+        return UpdateResponse(ticket=1, status=token)
+
+    def credential_validation(self, information: object) -> bool:
+        pass
 
     def get_server(self) -> FastAPI:
         router = FastAPI()
-        router.get("/")(self.hello_world)
+        router.get("/request/{cred_type}", response_model=RequestResponse)(self.recieve_credential_request)
+        router.get("/status/", response_model=UpdateResponse)(self.credential_status)
+        router.get("/")(self.landing)
         return router
---
+++ issuer-lib/issuer/models/responses.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class RequestResponse(BaseModel):
+    ticket: int
+    link: str
+
+# In theory used by the frontend to display status page
+class UpdateResponse(BaseModel):
+    ticket: int
+    status: str
