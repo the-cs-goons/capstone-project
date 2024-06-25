626c85b46a5ca4fb5eae1fb7dd3104e93f54f130
Ryan <apple1314156@gmail.com>
2024-06-25 10:06:00 +1000

    Removed hello-world boilerplate

--- owner-lib/owner/identity_owner.py
+++ owner-lib/owner/identity_owner.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
-
-from .models.hello_world import HelloWorldResponse
-
+import httpx
 
 class IdentityOwner:
-    async def hello_world(self) -> HelloWorldResponse:
-        return HelloWorldResponse(hello="Hello", world="World")
+    def __init__(self):
+        # self.credentials: dict[str, VerifiableCredential] = {}
+        pass
 
     def get_server(self) -> FastAPI:
         router = FastAPI()
-        router.get("/")(self.hello_world)
+        router.get("/")(self.hello)
+        router.get('/yomama')(self.f)
+        router.get("/pr")(self.temp_get_thingo)
         return router
+
+    async def hello(self):
+        return {"Hello" : "World"}
+
+    async def f(self):
+        return {"message": "YO MAMA FAT"}
+
+    async def temp_get_thingo(self, url):
+        client = httpx.AsyncClient()
+        response = await client.get(url)
+        return {'url': url}
--- owner-lib/tests/test_owner.py
+++ owner-lib/tests/test_owner.py
@@ -5,6 +5,5 @@ from owner import IdentityOwner
 @pytest.mark.asyncio
 async def test_hello_world():
     identity_owner = IdentityOwner()
-    response = await identity_owner.hello_world()
-    assert response.hello == "Hello"
-    assert response.world == "World"
+    # response = await identity_owner.hello_world()
+    assert True
