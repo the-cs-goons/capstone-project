0cbe766dbc0b06b813ab23994f7ac8eac92a71e8
Mack Cheesman <mack@thecheesmans.com>
2024-07-06 18:02:44 +1000

    newline gaming

--- vclib/owner/__init__.py
+++ vclib/owner/__init__.py
@@ -1,6 +1,7 @@
 """
 Identity Owner module
 """
+
 # Add imports from `owner/src` here to expose objects under vclib.owner
 from .src.identity_owner import IdentityOwner
 from .src.models.credentials import Credential
