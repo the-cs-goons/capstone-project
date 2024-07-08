f224602a96327fae340917af07e3938980dd8191
Mack Cheesman <mack@thecheesmans.com>
2024-07-09 00:27:01 +1000

    Created SDJWT func

--- vclib/common/src/sdjwt.py
+++ vclib/common/src/sdjwt.py
@@ -1,10 +1,21 @@
-from sd_jwt.common import SDJWTCommon
-from sd_jwt.issuer import SDJWTIssuer
+from ast import Dict
+from sd_jwt.common import SDObj
+from sd_jwt.issuer import SDJWTIssuer as SDJWT
 from jwcrypto.jwk import JWK
 
-def create_sdjwt_from_claims():
-    pass
 
+def create_sd_jwt(disclosable_claims: Dict, fields: Dict, signing_key: JWK):
+    """
+    Create a SD-JWT
+    """
+    payload = {}
 
-if __name__ == "__main__":
-    print(SDJWTIssuer.decoy_digests)
+    for key, value in disclosable_claims.items():
+        payload[SDObj(key)] = value
+    for key, value in fields:
+        payload[key] = value
+
+    # TODO: specific checking for mandatory fields that SDJWTIssuer does not enforce
+
+    sd_jwt = SDJWT(payload, signing_key, extra_header_parameters={"typ": "vc+sd-jwt"})
+    return sd_jwt
