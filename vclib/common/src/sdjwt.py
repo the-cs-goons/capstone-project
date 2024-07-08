ff014d98bec09395981871016ef36aaf09d73b03
Mack Cheesman <mack@thecheesmans.com>
2024-07-09 01:24:09 +1000

    Added exception test & linted

--- vclib/common/src/sdjwt.py
+++ vclib/common/src/sdjwt.py
@@ -1,12 +1,21 @@
-from ast import Dict
 from typing import Dict
+
+from jwcrypto.jwk import JWK
 from sd_jwt.common import SDObj
 from sd_jwt.issuer import SDJWTIssuer
-from jwcrypto.jwk import JWK
+
 
 class SDJWTVC(SDJWTIssuer):
+    """
+    Selective Disclosure JWT for Verifiable Credentials
+
+    Built upon the SDJWTIssuer class from `sd_jwt`.
+
+    TODO: Document further
+    """
 
     SD_JWT_HEADER = "vc+sd-jwt"
+    NONDISCLOSABLE_CLAIMS = ["iss", "nbf", "exp", "cnf", "vct", "status"]
 
     def __init__(self, 
                  disclosable_claims: Dict,
@@ -16,6 +25,10 @@ class SDJWTVC(SDJWTIssuer):
                  ):
         payload = {}
         for key, value in disclosable_claims.items():
+            # If the key of the claim is specifically of type SDJObj, 
+            # it will be included in _sd + as a digest
+            if key in self.NONDISCLOSABLE_CLAIMS:
+                raise Exception
             payload[SDObj(key)] = value
         for key, value in oth_claims.items():
             payload[key] = value
@@ -24,13 +37,6 @@ class SDJWTVC(SDJWTIssuer):
         # TODO: put the right stuff in the header
 
         super().__init__(payload, issuer_key, **kwargs)
-
-
-    def get_header(self):
-        pass
-
-    def get_payload(self):
-        pass
 
     def get_disclosures(self):
         return [digest.json for digest in self.ii_disclosures]
--- vclib/common/tests/test_sdjwtvc.py
+++ vclib/common/tests/test_sdjwtvc.py
@@ -1,22 +1,43 @@
-from vclib.common import SDJWTVC
-from jwcrypto.jwk import JWK
-
 from datetime import date, datetime
 from time import mktime
 
 import pytest
+from jwcrypto.jwk import JWK
+from jwcrypto.jws import JWS
+
+from vclib.common import SDJWTVC
+
 
 @pytest.fixture
 def jwk():
     return JWK(generate='EC')
 
-def test_create_credential(jwk):
-    disclosable_claims = {"given_name": "Bob", "family_name": "Jones", "dob": date.today().isoformat()}
+def test_create_and_verify_credential(jwk):
+    disclosable_claims = {
+        "given_name": "Bob", 
+        "family_name": "Jones", 
+        "dob": date.today().isoformat()}
     other = {"iat": mktime(datetime.now().timetuple())}
 
     new_credential = SDJWTVC(disclosable_claims, other, jwk)
 
-    assert len(new_credential.get_disclosures()) == 3
+    public_key = JWK.from_json(jwk.export_public())
 
+    disclosures: list = new_credential.get_disclosures()
+    assert len(disclosures) == 3
     
+    assert public_key.has_public
+    # Assert no exception gets raised
+    assert new_credential.sd_jwt.verify(public_key) is None
+    
+def test_registered_jwt_claims(jwk):
+    disclosable_claims = {
+        "given_name": "Bob", 
+        "family_name": "Jones", 
+        "iss": date.today().isoformat()}
+    other = {"iat": mktime(datetime.now().timetuple())}
+    with pytest.raises(Exception):
+        SDJWTVC(disclosable_claims, other, jwk)
+
+
 
