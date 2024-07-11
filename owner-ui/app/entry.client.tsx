9e1acdcbef39c6c4148ccf87f44a011bfa7dd89e
Lorenzo Grillo <z5360552@ad.unsw.edu.au>
2024-07-12 03:46:05 +1000

    reset frontend framework with MUI-compliant Remix setup and using pnpm

---
+++ owner-ui/app/entry.client.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { RemixBrowser } from '@remix-run/react';
+import { CacheProvider } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import ClientStyleContext from './src/ClientStyleContext';
+import createEmotionCache from './src/createEmotionCache';
+
+interface ClientCacheProviderProps {
+  children: React.ReactNode;
+}
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = React.useState(createEmotionCache());
+
+  const clientStyleContextValue = React.useMemo(
+    () => ({
+      reset() {
+        setCache(createEmotionCache());
+      },
+    }),
+    [],
+  );
+
+  return (
+    <ClientStyleContext.Provider value={clientStyleContextValue}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  );
+}
+
+const hydrate = () => {
+  React.startTransition(() => {
+    ReactDOM.hydrateRoot(
+      document,
+      <ClientCacheProvider>
+          <RemixBrowser />
+      </ClientCacheProvider>,
+    );
+  });
+};
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  setTimeout(hydrate, 1);
+}
