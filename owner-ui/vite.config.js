d22dd0e0fcd05185735deaad9077f0155c062703
Lorenzo Grillo <z5360552@ad.unsw.edu.au>
2024-07-13 23:22:46 +1000

    add coverage script

--- owner-ui/vite.config.js
+++ owner-ui/vite.config.js
@@ -2,6 +2,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { coverageConfigDefaults } from "vitest/config";
 
 export default defineConfig({
   plugins: [!process.env.VITEST && remix(), tsconfigPaths()],
@@ -9,5 +10,8 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./vitest.setup.ts",
+    coverage: {
+      exclude: [...coverageConfigDefaults.exclude, "build/**"],
+    },
   },
 });
