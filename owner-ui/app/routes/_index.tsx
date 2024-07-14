00487d4b0d504671a7068596d15fa27041792f12
Lorenzo Grillo <z5360552@ad.unsw.edu.au>
2024-07-15 00:55:59 +1000

    install Remix testing library; move credentials grid to separate endpoint

--- owner-ui/app/routes/_index.tsx
+++ owner-ui/app/routes/_index.tsx
@@ -1,12 +1,5 @@
-import {
-  Card,
-  Container,
-  Typography,
-  Unstable_Grid2 as Grid,
-} from "@mui/material";
-import { isRouteErrorResponse, useLoaderData } from "@remix-run/react";
-import { json } from "@remix-run/node";
-import type { MetaFunction, LoaderFunction } from "@remix-run/node";
+import { isRouteErrorResponse } from "@remix-run/react";
+import type { MetaFunction } from "@remix-run/react";
 
 export const meta: MetaFunction = ({ error }) => {
   let title = "SSI Wallet";
@@ -16,70 +9,11 @@ export const meta: MetaFunction = ({ error }) => {
       : "Error!";
   }
   return [
-    {
-      title: title,
-    },
+    { title: title },
     { name: "description", content: "Take control of your identity." },
   ];
 };
 
-export const loader: LoaderFunction = async () => {
-  const resp = await fetch(
-    `http://owner-lib:${process.env.CS3900_OWNER_AGENT_PORT}/credentials`,
-    { method: "GET" },
-  );
-  // TODO: make this more concrete
-  const data: {
-    id: string;
-    type: string;
-    issuer_name: string | null;
-    token: string | null;
-  }[] = await resp.json();
-  return json(data);
-};
-
 export default function Index() {
-  const credentials: {
-    id: string;
-    type: string;
-    issuer_name: string | null;
-    token: string | null;
-  }[] = useLoaderData();
-  return (
-    <Container component="main">
-      <Grid
-        container
-        columns={{ xs: 4, sm: 8, md: 12 }}
-        spacing={{ xs: 2, md: 3 }}
-        marginTop={{ xs: 1, md: 1.5 }}
-      >
-        {credentials.map((credential) => {
-          return (
-            <Grid
-              key={credential.id}
-              sx={{ display: "flex" }}
-              justifyContent={"center"}
-              size={4}
-            >
-              <Card>
-                <Typography variant="h4">{credential.type}</Typography>
-                <Typography variant="h6">{credential.issuer_name}</Typography>
-                {credential.token
-                  ? Object.entries(
-                      JSON.parse(atob(credential.token)) as [string, string][],
-                    ).map(([key, value]) => {
-                      return (
-                        <Typography variant="body1" key={key}>
-                          {value}
-                        </Typography>
-                      );
-                    })
-                  : "Pending approval"}
-              </Card>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </Container>
-  );
+  return <></>;
 }
---
+++ owner-ui/app/routes/credentials.tsx
@@ -0,0 +1,86 @@
+import {
+  Card,
+  Container,
+  Typography,
+  Unstable_Grid2 as Grid,
+} from "@mui/material";
+import type { LoaderFunction } from "@remix-run/node";
+import {
+  isRouteErrorResponse,
+  json,
+  useLoaderData,
+  type MetaFunction,
+} from "@remix-run/react";
+
+export const meta: MetaFunction = ({ error }) => {
+  let title = "Credentials - SSI Wallet";
+  if (error) {
+    title = isRouteErrorResponse(error)
+      ? `${error.status} ${error.statusText}`
+      : "Error!";
+  }
+  return [
+    {
+      title: title,
+    },
+    { name: "description", content: "View and manage credentials" },
+  ];
+};
+
+interface CredentialDataField {
+  id: string;
+  type: string;
+  issuer_name: string | null;
+  token: string | null;
+}
+
+export const loader: LoaderFunction = async () => {
+  const resp = await fetch(
+    `http://owner-lib:${process.env.CS3900_OWNER_AGENT_PORT}/credentials`,
+    { method: "GET" },
+  );
+  const data: Array<CredentialDataField> = await resp.json();
+  return json(data);
+};
+
+export default function Credentials() {
+  const credentials: Array<CredentialDataField> = useLoaderData();
+
+  return (
+    <Container component="main">
+      <Grid
+        container
+        columns={{ xs: 4, sm: 8, md: 12 }}
+        spacing={{ xs: 2, md: 3 }}
+        marginTop={{ xs: 1, md: 1.5 }}
+      >
+        {credentials.map((credential) => {
+          return (
+            <Grid
+              key={credential.id}
+              sx={{ display: "flex" }}
+              justifyContent={"center"}
+              size={4}
+            >
+              <Card>
+                <Typography variant="h4">{credential.type}</Typography>
+                <Typography variant="h6">{credential.issuer_name}</Typography>
+                {credential.token
+                  ? Object.entries(
+                      JSON.parse(atob(credential.token)) as [string, string][],
+                    ).map(([key, value]) => {
+                      return (
+                        <Typography variant="body1" key={key}>
+                          {value}
+                        </Typography>
+                      );
+                    })
+                  : "Pending approval"}
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
+}
