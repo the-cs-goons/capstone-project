339b8c6a2cf7e8da3731496c45d80bef5e2f24d1
Lorenzo Grillo <z5360552@ad.unsw.edu.au>
2024-07-16 03:09:57 +1000

    move bottom navigation to layout route

--- owner-ui/app/routes/_auth.credentials.tsx
+++ owner-ui/app/routes/_auth.credentials.tsx
@@ -1,9 +1,4 @@
-import {
-  AccountBalanceWallet as AccountBalanceWalletIcon,
-  Add as AddIcon,
-  Logout as LogoutIcon,
-  QrCode as QrCodeIcon,
-} from "@mui/icons-material";
+import { Add as AddIcon } from "@mui/icons-material";
 import {
   Typography,
   Unstable_Grid2 as Grid,
@@ -11,12 +6,6 @@ import {
   Toolbar,
   Tooltip,
   IconButton,
-  BottomNavigation,
-  BottomNavigationAction,
-  Paper,
-  useScrollTrigger,
-  Slide,
-  SlideProps,
 } from "@mui/material";
 import type { LoaderFunction } from "@remix-run/node";
 import {
@@ -25,7 +14,6 @@ import {
   useLoaderData,
   type MetaFunction,
 } from "@remix-run/react";
-import { useState } from "react";
 import { CredentialCard } from "~/components/CredentialCard";
 import { CredentialsGridContainer } from "~/components/CredentialsGridContainer";
 import { FlexContainer } from "~/components/FlexContainer";
@@ -61,19 +49,8 @@ export const loader: LoaderFunction = async () => {
   return json(data);
 };
 
-function HideOnScroll({ children }: Readonly<SlideProps>) {
-  const trigger = useScrollTrigger();
-
-  return (
-    <Slide appear={false} direction="up" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
-
 export default function Credentials() {
   const credentials: Array<CredentialDataField> = useLoaderData();
-  const [value, setValue] = useState(0);
 
   return (
     <>
@@ -94,7 +71,7 @@ export default function Credentials() {
           </Tooltip>
         </Toolbar>
       </AppBar>
-      <FlexContainer component="main" maxWidth="xl" disableGutters={true}>
+      <FlexContainer component="main" maxWidth="xl" disableGutters>
         <CredentialsGridContainer>
           <Grid
             container
@@ -117,26 +94,6 @@ export default function Credentials() {
           </Grid>
         </CredentialsGridContainer>
       </FlexContainer>
-      <HideOnScroll>
-        <Paper
-          elevation={8}
-          sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-        >
-          <BottomNavigation
-            showLabels
-            component="nav"
-            value={value}
-            onChange={(_event, newValue) => setValue(newValue)}
-          >
-            <BottomNavigationAction
-              label="Wallet"
-              icon={<AccountBalanceWalletIcon />}
-            />
-            <BottomNavigationAction label="Scan QR" icon={<QrCodeIcon />} />
-            <BottomNavigationAction label="Logout" icon={<LogoutIcon />} />
-          </BottomNavigation>
-        </Paper>
-      </HideOnScroll>
     </>
   );
 }
---
+++ owner-ui/app/routes/_auth.scan.tsx
@@ -0,0 +1,71 @@
+import { AddLink as AddLinkIcon } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { isRouteErrorResponse, MetaFunction } from "@remix-run/react";
+import { Scanner } from "@yudiel/react-qr-scanner";
+import { FlexContainer } from "~/components/FlexContainer";
+
+export const meta: MetaFunction = ({ error }) => {
+  let title = "Scan QR Code - SSI Wallet";
+  if (error) {
+    title = isRouteErrorResponse(error)
+      ? `${error.status} ${error.statusText}`
+      : "Error!";
+  }
+  return [
+    {
+      title: title,
+    },
+    { name: "description", content: "View and manage credentials" },
+  ];
+};
+
+export default function Scan() {
+  return (
+    <>
+      <AppBar position="sticky">
+        <Toolbar sx={{ pb: 2, pt: 1, minHeight: 128, alignItems: "flex-end" }}>
+          <Typography
+            variant="h4"
+            component="h1"
+            id="credentials-appbar-title"
+            flexGrow={1}
+          >
+            Scan QR code
+          </Typography>
+          <Tooltip id="enter-link-tooltip" title="Type Credential link">
+            <IconButton color="inherit" aria-label="Type Credential link">
+              <AddLinkIcon />
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      </AppBar>
+      <FlexContainer component="main" maxWidth="xl" disableGutters>
+        <Box
+          sx={{
+            textAlign: "center",
+            width: "100%",
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <Scanner
+            styles={{
+              container: { height: "100%" },
+              finderBorder: 50,
+            }}
+            onScan={(result) => console.log(result)}
+          />
+        </Box>
+      </FlexContainer>
+    </>
+  );
+}
