d94837a84bd255b4dfe3f2ede5aa8790a815907b
Zefeng Fei <z5089786@ad.unsw.edu.au>
2024-07-29 19:43:27 +1000

    add consecutive snackbar

--- owner-ui/app/routes/_auth.present.tsx
+++ owner-ui/app/routes/_auth.present.tsx
@@ -45,23 +45,47 @@ export async function action({ request }: ActionFunctionArgs) {
   }
 }
 
+export interface SnackbarMessage {
+  message: string;
+  key: number;
+}
+
 export default function Present() {
   const data = useActionData<typeof action>();
   const definition = data?.presentation_definition;
   const submit = useSubmit();
 
+  const [snackPack, setSnackPack] = React.useState<readonly SnackbarMessage[]>([]);
   const [open, setOpen] = React.useState(false);
+  const [messageInfo, setMessageInfo] = React.useState<SnackbarMessage | undefined>(
+    undefined,
+  );
 
-  const handleClick = () => {
-    setOpen(true);
+  React.useEffect(() => {
+    if (snackPack.length && !messageInfo) {
+      // Set a new snack when we don't have an active one
+      setMessageInfo({ ...snackPack[0] });
+      setSnackPack((prev) => prev.slice(1));
+      setOpen(true);
+    } else if (snackPack.length && messageInfo && open) {
+      // Close an active snack when a new one is added
+      setOpen(false);
+    }
+  }, [snackPack, messageInfo, open]);
+
+  const handleClick = (message: string) => () => {
+    setSnackPack((prev) => [...prev, { message, key: new Date().getTime() }]);
   };
 
   const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
     if (reason === 'clickaway') {
       return;
     }
-
     setOpen(false);
+  };
+
+  const handleExited = () => {
+    setMessageInfo(undefined);
   };
 
   function handlePresent(event: FormEvent<HTMLFormElement>): void {
@@ -118,15 +142,19 @@ export default function Present() {
             </Paper>
           );
         })}
-        <Button onClick={handleClick} type="submit" name="intent" value="present-cred">
+        <Button type="submit" name="intent" value="present-cred">
           Present
         </Button>
         <div>
-          <Button onClick={handleClick}>Open Snackbar</Button>
+          <Button onClick={handleClick("pending")}>Open Pending Snackbar</Button>
+          <Button onClick={handleClick("success")}>Open Success Snackbar</Button>
           <Snackbar
+            key={messageInfo ? messageInfo.key : undefined}
             open={open}
             autoHideDuration={6000}
             onClose={handleClose}
+            TransitionProps={{ onExited: handleExited }}
+            message={messageInfo ? messageInfo.message : undefined}
           >
             <Alert
               onClose={handleClose}
@@ -134,7 +162,7 @@ export default function Present() {
               variant="filled"
               sx={{ width: '100%' }}
             >
-              Verification Pending...
+              {messageInfo ? messageInfo.message : undefined}
             </Alert>
           </Snackbar>
         </div>
