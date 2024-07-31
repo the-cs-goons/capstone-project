d8ddec061d7a4db06b7ee5967ec1b3c8c66c3cb7
Zefeng Fei <z5089786@ad.unsw.edu.au>
2024-07-31 11:01:07 +1000

    fix padding

--- owner-ui/app/routes/_auth.present.tsx
+++ owner-ui/app/routes/_auth.present.tsx
@@ -1,6 +1,7 @@
-import { Button, FormControlLabel, Paper, Switch } from "@mui/material";
+import { ArrowBack as ArrowBackIcon } from "@mui/icons-material";
+import { AppBar, Button, FormControlLabel, IconButton, Paper, Switch, Toolbar, Tooltip, Typography } from "@mui/material";
 import { json, SerializeFrom, type ActionFunctionArgs } from "@remix-run/node";
-import { Form, redirect, useActionData, useSubmit } from "@remix-run/react";
+import { Form, redirect, useActionData, useNavigate, useSubmit } from "@remix-run/react";
 import { AxiosResponse } from "axios";
 import type { FormEvent } from "react";
 import { FlexContainer } from "~/components/FlexContainer";
@@ -44,6 +45,7 @@ export default function Present() {
   const data = useActionData<typeof action>();
   const definition = data?.presentation_definition;
   const submit = useSubmit();
+  const navigate = useNavigate();
 
   function handlePresent(event: FormEvent<HTMLFormElement>): void {
     event.preventDefault();
@@ -75,12 +77,32 @@ export default function Present() {
     );
   }
 
-  return (
+  return (<>
+    <AppBar position="sticky">
+        <Toolbar sx={{ pb: 2, pt: 1, minHeight: 128, alignItems: "flex-end" }}>
+    <IconButton
+            color="inherit"
+            aria-label="Back"
+            onClick={() => navigate(-1)}
+          >
+            <ArrowBackIcon />
+          </IconButton>
+          <Typography
+            variant="h4"
+            component="h1"
+            id="credentials-appbar-title"
+            flexGrow={1}
+          >
+            Choose information
+          </Typography>
+        </Toolbar>
+      </AppBar>
     <FlexContainer component="main" maxWidth="xl">
       <Form method="post" onSubmit={handlePresent}>
+        <Typography>{data?.client_id} is requesting the following data.</Typography>
         {definition?.input_descriptors.map((input_descriptor) => {
           return (
-            <Paper key={input_descriptor.id}>
+            <Paper key={input_descriptor.id} sx={{p: 3}}>
               <FormControlLabel
                 label={input_descriptor.name ?? input_descriptor.id}
                 control={
@@ -100,7 +122,8 @@ export default function Present() {
           );
         })}
         <Button type="submit">Present</Button>
+        <Button type="button" color="error" onClick={() => navigate(-1)}>Reject</Button>
       </Form>
-    </FlexContainer>
+    </FlexContainer></>
   );
 }
